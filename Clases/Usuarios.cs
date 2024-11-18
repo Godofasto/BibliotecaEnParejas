6133da9c218ebbdf9c71da0834b66f27c6c30461
hidde <hiddennymeria@gmail.com>
2024-11-18 12:09:13 +0100

    erg

--- Clases/Usuarios.cs
+++ Clases/Usuarios.cs
@@ -11,10 +11,25 @@ namespace BibliotecaEnParejas.Clases
         private String tipo;
         private String nombre;
         private String departamento;
+        private DateTime fechaSancion;  //Revisar si las fechas se ponen asi
 
         public Usuarios() { }
 
-        
-
+        public void setTipo(String tip)
+        {
+            this.tipo = tip;
+        }
+        public void setNombre(String nom)
+        {
+            this.nombre = nom;
+        }
+        public void setDepartamento(String depar)
+        {
+            this.departamento = depar;
+        }
+        public void setFechaSancion(DateTime fechaSan)
+        {
+            this.fechaSancion = fechaSan;
+        }
     }
 }
