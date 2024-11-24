a4f0df207ae94a3534ed581804712a93e428f5e9
David García Ramírez <garciaramirezdavid23@gmail.com>
2024-11-24 13:59:59 +0100

    cipote

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ namespace BibliotecaEnParejas
         {
             InitializeComponent();
             //aohfosdfosdifcvddddd
+            //cipote
             MessageBox.Show("aaa");
             MessageBox.Show("aaa");
             MessageBox.Show("aaa");
